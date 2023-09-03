c166a5e981e66cf2dd6f8cadcea446934017617f
tre torrence <tretorrence1@gmail.com>
2023-09-03 18:01:39 -0400

    Updated socials links in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -79,13 +79,13 @@ const Navbar = () => {
         <ul>
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600" >
                 <a className="flex justify-between items-center w-full text-gray-300"
-                 href="/">
+                 href="https://www.linkedin.com/in/tre-torrence/" target='_blank' rel='noopener noreferrer'>
                     LinkedIn <FaLinkedin size={30} />
                 </a>
             </li>
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]" >
                 <a className="flex justify-between items-center w-full text-gray-300"
-                 href="/">
+                 href="https://github.com/KuznRo" target='_blank' rel='noopener noreferrer'>
                     Github <FaGithub size={30} />
                 </a>
             </li>
